602a2af0c1f1d8044fcc91978ef23ac896847ee7
GonzaloTI <ignaciogonzaoid@gmail.com>
2025-11-20 15:31:44 -0400

    pruebatiktok

--- sender.py
+++ sender.py
@@ -46,35 +46,35 @@ class Sender:
             results = {}
 
             # WhatsApp
-            if "whatsapp" in data and "response" in data["whatsapp"]:
-                message = data["whatsapp"]["response"]
+            # if "whatsapp" in data and "response" in data["whatsapp"]:
+            #     message = data["whatsapp"]["response"]
                 
                 
-                message=self.whatsapp.send_message(message_body=message,to_number="whatsapp:+59167769632")
+            #     message=self.whatsapp.send_message(message_body=message,to_number="whatsapp:+59167769632")
                 
-                results["whatsapp"] = message.sid
-                logger.info("enviado por whatsapp")
+            #     results["whatsapp"] = message.sid
+            #     logger.info("enviado por whatsapp")
 
-            # # Facebook
-            if "facebook" in data and "response" in data["facebook"]:
-                message = data["facebook"]["response"]
-                results["facebook"] = self.facebook.publicar_texto(message)
+            # # # Facebook
+            # if "facebook" in data and "response" in data["facebook"]:
+            #     message = data["facebook"]["response"]
+            #     results["facebook"] = self.facebook.publicar_texto(message)
 
-            # # Instagram
-            if "instagram" in data and "response" in data["instagram"]:
-                message = data["instagram"]["response"]
-                results["instagram"] = self.instagram.publicar(caption=message,image_url="https://mrmoviliano.com/wp-content/uploads/2020/01/jfif.jpg")
+            # # # Instagram
+            # if "instagram" in data and "response" in data["instagram"]:
+            #     message = data["instagram"]["response"]
+            #     results["instagram"] = self.instagram.publicar(caption=message,image_url="https://mrmoviliano.com/wp-content/uploads/2020/01/jfif.jpg")
 
-            # # LinkedIn
-            if "linkedin" in data and "response" in data["linkedin"]:
-                message = data["linkedin"]["response"]
-                results["linkedin"] = self.linkedin.publicar(message)
+            # # # LinkedIn
+            # if "linkedin" in data and "response" in data["linkedin"]:
+            #     message = data["linkedin"]["response"]
+            #     results["linkedin"] = self.linkedin.publicar(message)
 
             # # TikTok
             if "tiktok" in data and "response" in data["tiktok"]:
                 message = data["tiktok"]["response"]
-                #results["tiktok"] = self.tiktok.publicar_imagen_url_old_domain_very( texto=message,imagen_url="https://pagina-de-presentacion3.onrender.com/images/flores.jpg")
-                results["tiktok"] = self.tiktok.publicar_imagen_url( texto=message,imagen_url="https://mrmoviliano.com/wp-content/uploads/2020/01/jfif.jpg")
+                results["tiktok"] = self.tiktok.publicar_imagen_url_old_domain_very( texto=message,imagen_url="https://pagina-de-presentacion3.onrender.com/images/flores.jpg")
+                #results["tiktok"] = self.tiktok.publicar_imagen_url( texto=message,imagen_url="https://mrmoviliano.com/wp-content/uploads/2020/01/jfif.jpg")
 
             logger.info("Mensajes enviados a todas las plataformas disponibles.")
             return results
